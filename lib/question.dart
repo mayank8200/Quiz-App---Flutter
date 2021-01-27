1a4d58ea2a3cc1a6df75667d09cebd99870fcd94
Mayank Bajaj <41484766+mayank8200@users.noreply.github.com>
2021-01-27 14:17:13 +0530

    Added Styling to the question text

--- lib/question.dart
+++ lib/question.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+
 class Question extends StatelessWidget {
   final String question;
   Question(this.question);
   @override
   Widget build(BuildContext context) {
-    return Text(question);
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(10),
+        child: Text(question,
+            style: TextStyle(fontSize: 30), textAlign: TextAlign.center));
   }
 }
